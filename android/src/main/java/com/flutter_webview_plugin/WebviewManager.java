83b59c483f2079fa29d0d92e616d1da0ad4d2196
Johannes Ylönen <johannes.ylonen@indium.fi>
2020-03-06 20:02:07 +0200

    Allow video and microphone permissions automatically.

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
 import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
 import android.webkit.SslErrorHandler;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -265,6 +266,14 @@ class WebviewManager {
 
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
                 callback.invoke(origin, true, false);
+            }
+            
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    // Allow permissions automatically
+                    request.grant(request.getResources());
+                }
             }
         });
         registerJavaScriptChannelNames(channelNames);
